6c9aaa3bd07407bf80df3829189960aae3eefa22
Lance Atherton <lchoppa.ssmaniac@live.com>
2023-09-26 08:18:22 -0500

    Saving all Python and moving to git and github

---
+++ Learning Python/CrashCourse.py
@@ -0,0 +1,9 @@
+# This is a single line comment
+"""This is a 
+Multiline Comment"""
+
+# variables
+a = "Var A" # Single
+b = ["Var B_1", "Var B_2"] # List
+b[1] #returns first variable in list
+b + [1,2,3,4,5] #Adds new list to Var B
